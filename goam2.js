b675d79555942aa625cce28c05da95825ced5a81
porosadporosad <tmxk1594@naver.com>
2024-01-13 22:49:26 +0900

    삭제 기능 수정

--- goam2.js
+++ goam2.js
@@ -13,19 +13,94 @@ const replyList = []; // 리뷰 작성 값을 담기 위해 전역 배열로 선
 // 함수안에 지역변수로 선언하면 함수가 호출될 때마다 지역변수가 새로 정의되고 초기화 됨
 // 그래서 전역변수로 선언하여 계속 배열에 담는다.
 
-console.log("replyData", replyData);
 
 // 저장된 댓글 페이지에 출력
 if (replyData != null) {
   replyData.forEach((data, idx) => {
-    const newComment = document.createElement("div");
-    newComment.className = "comment";
-    newComment.innerHTML = `
-          <div class="input-group">
-          <span class="input-group-text"><strong>${data.nickname}</strong><span>${data.rating}</span></span>
-          <textarea class="form-control" aria-label="With textarea" disabled>${data.commentText}</textarea>
-          <button type="button" class="btn btn-outline-dark" id="deleteBtn">삭제</button>
-          </div>`;
+            // div
+            const newComment = document.createElement("div");
+            newComment.setAttribute("class", "input-group comment");
+            // span
+            const $readSpanStrong = document.createElement("span");
+            $readSpanStrong.setAttribute("class", "input-group-text");
+            $readSpanStrong.innerHTML = `<strong>${data.nickname}</strong><span>${data.rating}</span>`;
+            // textarea
+            const $readTextarea = document.createElement("textarea");
+            $readTextarea.setAttribute("class", "form-control");
+            $readTextarea.setAttribute("aria-label", "With textarea");
+            $readTextarea.setAttribute("disabled", "true");
+            $readTextarea.innerText = `${data.commentText}`;
+            // 버튼만들기
+            const $readButton = document.createElement("button");
+            $readButton.setAttribute("type", "button");
+            $readButton.setAttribute("class", "btn btn-outline-dark");
+            $readButton.addEventListener("click", function () {
+              // 선택된 버튼의 부모태그 
+              const parentElement = $readButton.closest('.input-group');
+              // 선택된 태그 안의 이름, 평점, 리뷰 가져오기
+              const nicknameElement = parentElement.querySelector('strong');
+              const ratingElement = parentElement.querySelector('span span');
+              const commentTextElement = parentElement.querySelector('textarea');
+
+              // 로컬스토리지에서 선택된 값
+              const data = JSON.parse(localStorage.getItem(movieId));
+              // 이름, 평점, 리뷰 내용 가져오기
+              const nickname = nicknameElement.textContent;
+              const rating = ratingElement.textContent;
+              const commentText = commentTextElement.value;
+
+              let realData = 0;
+
+              // 선택된 태그와 로컬스토리지의 값이 같은거 찾기
+              for (let i = 0; i < data.length; i++) {
+                const item = data[i];
+                if (item.nickname === nickname && item.rating === rating && item.commentText === commentText) {
+                  realData = i;
+                  break;
+                }
+              }
+              
+              // data 값의 패스워드 가져오기
+              const pw = data[realData].pw
+          
+              function delRev(){
+                const result = confirm("삭제하시겠습니까?");
+                if(!result){
+                  alert("취소되었습니다");
+                  return;
+                }
+          
+                const pwtext = prompt("비밀번호를 입력하세요");
+                if (!pwtext) {
+                  alert("비밀번호가 필요합니다");
+                  return;
+                }
+          
+                if(pwtext !== pw){
+                  alert("비밀번호가 틀렸습니다");
+                  return;
+                }
+          
+                const onemore = confirm("정말 삭제하시겠습니까?");
+                if(!onemore){
+                  alert("취소되었습니다");
+                  return;
+                }
+                // 보통 리뷰 쓰다가 지우면 새로고침은 되지않고 그거만 지워지니 지워지도록 보여주기위해 display none
+                parentElement.style.display = 'none';
+                // 선택된 데이터 지우기
+                data.splice(realData, 1);
+                // 남은 데이터 최신화
+                localStorage.setItem(movieId, JSON.stringify(data));
+              }
+              delRev();
+            });
+            $readButton.innerText = `삭제`;
+            
+            // 위에 만든것들 각각 넣기
+            newComment.append($readSpanStrong);
+            newComment.append($readTextarea);
+            newComment.append($readButton);
 
     const commentsList = document.getElementById("commentsList");
     commentsList.appendChild(newComment);
@@ -69,16 +144,89 @@ document.getElementById("reply").addEventListener("click", function (event) {
     if (nickname.length > 5) {
       alert("너무길엉~");
       return;
-    }
+    }       // div
+            const newComment = document.createElement("div");
+            newComment.setAttribute("class", "input-group comment");
+            // span
+            const $readSpanStrong = document.createElement("span");
+            $readSpanStrong.setAttribute("class", "input-group-text");
+            $readSpanStrong.innerHTML = `<strong>${nickname}</strong><span>${rating}</span>`;
+            // textarea
+            const $readTextarea = document.createElement("textarea");
+            $readTextarea.setAttribute("class", "form-control");
+            $readTextarea.setAttribute("aria-label", "With textarea");
+            $readTextarea.setAttribute("disabled", "true");
+            $readTextarea.innerText = `${commentText}`;
+            // 버튼만들기
+            const $readButton = document.createElement("button");
+            $readButton.setAttribute("type", "button");
+            $readButton.setAttribute("class", "btn btn-outline-dark");
+            $readButton.addEventListener("click", function () {
+              // 선택된 버튼의 부모태그 
+              const parentElement = $readButton.closest('.input-group');
+              // 선택된 태그 안의 이름, 평점, 리뷰 가져오기
+              const nicknameElement = parentElement.querySelector('strong');
+              const ratingElement = parentElement.querySelector('span span');
+              const commentTextElement = parentElement.querySelector('textarea');
 
-    const newComment = document.createElement("div"); // createElement 태그 요소를 만든다.
-    newComment.className = "comment"; //newComment.className <div> 클래스네임은 말그대로 클래스 네임
-    newComment.innerHTML = `
-          <div class="input-group">
-          <span class="input-group-text"><strong>${nickname}</strong><span>${rating}</span></span>
-          <textarea class="form-control" aria-label="With textarea" disabled>${commentText}</textarea>
-          <button type="button" class="btn btn-outline-dark" id="deleteBtn">삭제</button>
-          </div>`;
+              // 로컬스토리지에서 선택된 값
+              const data = JSON.parse(localStorage.getItem(movieId));
+              // 이름, 평점, 리뷰 내용 가져오기
+              const nickname = nicknameElement.textContent;
+              const rating = ratingElement.textContent;
+              const commentText = commentTextElement.value;
+
+              let realData = 0;
+
+              // 선택된 태그와 로컬스토리지의 값이 같은거 찾기
+              for (let i = 0; i < data.length; i++) {
+                const item = data[i];
+                if (item.nickname === nickname && item.rating === rating && item.commentText === commentText) {
+                  realData = i;
+                  break;
+                }
+              }
+              
+              // data 값의 패스워드 가져오기
+              const pw = data[realData].pw
+          
+              function delRev(){
+                const result = confirm("삭제하시겠습니까?");
+                if(!result){
+                  alert("취소되었습니다");
+                  return;
+                }
+          
+                const pwtext = prompt("비밀번호를 입력하세요");
+                if (!pwtext) {
+                  alert("비밀번호가 필요합니다");
+                  return;
+                }
+          
+                if(pwtext !== pw){
+                  alert("비밀번호가 틀렸습니다");
+                  return;
+                }
+          
+                const onemore = confirm("정말 삭제하시겠습니까?");
+                if(!onemore){
+                  alert("취소되었습니다");
+                  return;
+                }
+                // 보통 리뷰 쓰다가 지우면 새로고침은 되지않고 그거만 지워지니 지워지도록 보여주기위해 display none
+                parentElement.style.display = 'none';
+                // 선택된 데이터 지우기
+                data.splice(realData, 1);
+                // 남은 데이터 최신화
+                localStorage.setItem(movieId, JSON.stringify(data));
+              }
+              delRev();
+            });
+            $readButton.innerText = `삭제`;
+
+            newComment.append($readSpanStrong);
+            newComment.append($readTextarea);
+            newComment.append($readButton);
 
     const commentsList = document.getElementById("commentsList");
     commentsList.appendChild(newComment); // appendChild <태그>안에 요소를 하나씩만 넣을 수 있다.
@@ -90,11 +238,16 @@ document.getElementById("reply").addEventListener("click", function (event) {
       commentText
     }; // 여러가지 데이터를 하나에 넣을 수 있다.
 
+    // function localVal(){
+
+    // }
+
     replyList.push(replyData); // 인덱스안에 obj를 넣는다. [{}, {}, {}, {}, {}, {}....]
     localStorage.setItem(movieId, JSON.stringify(replyList));
     // localstorage 바구니안에 동일한 key이름으로는 1개의 value만 담을 수 있는데 전역변수로 선언 한 배열 하나만 넣는다
     // 그 배열의 안에 객체(각각의 영화리뷰 1개씩을 뜻함)를 push해서 담아도 배열 1개 즉 value 1개만 된다.
-    
+    // history.scrollRestoration = "auto";
+    // location.reload(true);
 
   }
 });
@@ -176,53 +329,3 @@ fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?language=ko`, reques
     });
   })
   .catch((err) => console.error(err));
-
-// 삭제 버튼들 선택
-const deleteBtns = document.querySelectorAll("#deleteBtn");
-
-// 삭제기능
-deleteBtns.forEach((deleteBtn, index) => {
-  deleteBtn.addEventListener("click", function () {
-    // 선택된 버튼의 부모태그 
-    const parentElement = deleteBtns[index].closest('.input-group');
-    // 로컬스토리지에서 선택된 값
-    const data = JSON.parse(localStorage.getItem(movieId));
-    
-    // data 값의 패스워드 가져오기
-    const pw = data[0].pw
-
-    function delRev(){
-      const result = confirm("삭제하시겠습니까?");
-      if(!result){
-        alert("취소되었습니다");
-        return;
-      }
-
-      const pwtext = prompt("비밀번호를 입력하세요");
-      if (!pwtext) {
-        alert("비밀번호가 필요합니다");
-        return;
-      }
-
-      if(pwtext !== pw){
-        alert("비밀번호가 틀렸습니다");
-        return;
-      }
-
-      const onemore = confirm("정말 삭제하시겠습니까?");
-      if(!onemore){
-        alert("취소되었습니다");
-        return;
-      }
-      // 보통 리뷰 쓰다가 지우면 새로고침은 되지않고 그거만 지워지니 지워지도록 보여주기위해 display none
-      parentElement.style.display = 'none';
-      // 선택된 데이터 지우기
-      data.splice(index, 1);
-      // 남은 데이터 최신화
-      localStorage.setItem(movieId, JSON.stringify(data));
-    }
-    delRev();
-  });
-});
-
-
